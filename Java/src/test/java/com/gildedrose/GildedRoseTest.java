562a7f82629c8126f5e1894bf4a9a9b28df48846
ellenmae <ellenh@iastate.edu>
2019-05-05 13:40:08 -0500

    Initial commit, commented out updateQuality to refactor

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,11 @@
-package com.gildedrose;
-
-import static org.junit.Assert.*;
+package src.test.java.com.gildedrose;
 
 import org.junit.Test;
+import src.main.java.com.gildedrose.GildedRose;
+import src.main.java.com.gildedrose.Item;
+
+import static org.junit.Assert.*;
+import src.main.java.com.gildedrose.Item;
 
 public class GildedRoseTest {
 
@@ -11,7 +14,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("fixme", app.items[0].name); // create getter/setters
     }
 
 }
