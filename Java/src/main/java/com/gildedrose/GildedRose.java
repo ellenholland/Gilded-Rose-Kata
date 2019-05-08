8ef2a8d6349d01e065b2d9f7d3bf9b84c09e0810
Luke <Luke@DESKTOP-0FHLUD7>
2019-05-07 20:07:54 -0500

    refactoring code, only one test failing, need to account for other category. thinking about adding a hashmap but first - gym break

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,63 +2,43 @@ package src.main.java.com.gildedrose;
 
 public class GildedRose {
 
-    enum quality{
-        MAX_QUALITY(50);
-        private int quality;
-        quality(int quality){
-            this.quality = quality;
-        }
-    }
-
-
-    enum itemCategory{
-        AGED("Aged Brie"),
-        LEGENDARY("Sulfuras, Hand of Ragnaros"),
-        PASSES("Backstage passes to a TAFKAL80ETC concert"),
-        CONJURED("Conjured Mana Cake");
-        private String category;
-        public String getCategory() {
-            return this.category;
-        }
-        itemCategory(String category) {
-            this.category = category;
-        }
-    }
-
     public Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
-
     public void updateQuality() {
         for(Item item:items) {
             if(item.name.equals("Aged Brie")){
                 updateAGED(item);
-                updateSellIn(item);
             }
             if(item.name.equals("Sulfuras, Hand of Ragnaros")){
                 updateLEGENDARY(item);
-                updateSellIn(item);
             }
             if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
                 updatePASSES(item);
-                updateSellIn(item);
             }
             if(item.name.equals("Conjured Mana Cake")){
                 updateCONJURED(item);
-                updateSellIn(item);
             }
             updateSellIn(item);
         }
         // if quality is negative, make 0.
         // if quality is more than 50, unless legendary
     }
+
     public void updateSellIn(Item item){
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.setSellIn(item.getSellIn()-1);
         }
+    }
+
+    public int increaseQuality(Item item){
+        if(item.getQuality() < 50){
+            item.setQuality(item.getQuality()+1);
+        }
+        return item.getQuality();
     }
 
     public void updateAGED(Item item){
@@ -74,13 +54,12 @@ public class GildedRose {
         // quality increases by 3 when there are 5 days or less
         // quality = 0 after concert
 
-        if(item.getSellIn()>10){
+        item.setQuality(increaseQuality(item));
+
+        if(item.getSellIn() <= 10){
             item.setQuality(increaseQuality(item));
         }
-        else if(item.getSellIn() <= 10){
-            item.setQuality(increaseQuality(item));
-        }
-        else if(item.getSellIn() <= 5){
+        if(item.getSellIn() <= 5){
             item.setQuality(increaseQuality(item));
         }
         if(item.sellIn <= 0){
@@ -89,14 +68,9 @@ public class GildedRose {
     }
     public void updateCONJURED(Item item){
         // degrades in quality twice as fast
+        item.setQuality(item.getQuality()-2);
     }
-    
-    public int increaseQuality(Item item){
-        if(item.getQuality() < 50){
-            item.setQuality(item.getQuality()+1);
-        }
-        return item.getQuality();
-    }
+
     // other:
     // once sell by date has passed quality degrades twice as fast
 }
